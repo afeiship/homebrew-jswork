9fa533a29c2421c264890758aae465453ee939fb
afeiship <1290657123@qq.com>
2022-01-06 22:44:22 +0800

    fix: rm reviersion

--- Formula/t2k.rb
+++ Formula/t2k.rb
@@ -1,6 +1,6 @@
 class T2k < Formula
   homepage "https://github.com/afeiship/topics2keywords"
-  url "https://github.com/afeiship/topics2keywords", :using => :git, :revision => "3fffe8b784d174cce6594429f11eecc9b226251f"
+  url "https://github.com/afeiship/topics2keywords", :using => :git
   version "1.0.1"
 
   def install
