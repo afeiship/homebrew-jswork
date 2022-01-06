b989ed9c230bea2b17195cee7c654b978f6840b0
afeiship <1290657123@qq.com>
2022-01-06 22:39:14 +0800

    fix: git reversion

--- Formula/t2k.rb
+++ Formula/t2k.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class T2k < Formula
   homepage "https://github.com/afeiship/topics2keywords"
-  url "https://github.com/afeiship/topics2keywords", :using => :git
+  url "https://github.com/afeiship/topics2keywords", :using => :git, :revision => "3fffe8b784d174cce6594429f11eecc9b226251f"
   version "1.0.1"
 
   def install
