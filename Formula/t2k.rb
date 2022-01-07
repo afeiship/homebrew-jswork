840c74f1684a760fae1d5b862e037767ba7cba2a
afeiship <1290657123@qq.com>
2022-01-07 13:09:44 +0800

    test: install script

--- Formula/t2k.rb
+++ Formula/t2k.rb
@@ -7,6 +7,8 @@ class T2k < Formula
 
   def install
     puts ENV
+    puts Dir.pwd
+    puts ENV["GITHUB_API_TOKEN"]
     system "npm", "--version"
     puts "install end."
     # system "npm", "i", "-g", "@jswork/topics2keywords"
