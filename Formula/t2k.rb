054c54d651b7ccbfdee4537752e25f647e9a57a5
afeiship <1290657123@qq.com>
2022-01-07 13:07:26 +0800

    test: install script

--- Formula/t2k.rb
+++ Formula/t2k.rb
@@ -1,3 +1,5 @@
+require "formula"
+
 class T2k < Formula
   homepage "https://github.com/afeiship/topics2keywords"
   url "https://github.com/afeiship/topics2keywords", :using => :git
@@ -6,6 +8,7 @@ class T2k < Formula
   def install
     puts ENV
     system "npm", "--version"
+    puts "install end."
     # system "npm", "i", "-g", "@jswork/topics2keywords"
   end
 
