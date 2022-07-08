75319d0f006e36c75bb7530639f00f710e429799
afeiship <1290657123@qq.com>
2022-07-08 14:23:30 +0800

    wip

--- Formula/t2k.rb
+++ Formula/t2k.rb
@@ -1,16 +1,14 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class T2k < Formula
   desc "Sync github topics to npm keywords."
-  version "1.0.4"
   homepage "https://js.work"
-  url "https://github.com/afeiship/topics2keywords", :using => :git
+  url "https://github.com/afeiship/topics2keywords/archive/refs/tags/1.0.1.tar.gz"
+  sha256 "90b81bf4bd1675dc780c974119caa6c904c23bca9a572233c4c8fc3898df83da"
+  license "MIT"
 
   def install
-    puts "Installing t2k..."
-    #  安装不成功，暂时放弃。
-    system "npm install && npm link"
-  end
-
-  test do
-    system "t2k --version"
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
   end
 end
