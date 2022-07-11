4cff670a8c80227464a826de8db2010dd23e1b00
afeiship <1290657123@qq.com>
2022-07-11 21:54:36 +0800

    chore: add aliyun-cli version

--- Formula/aliyun-cli.rb
+++ Formula/aliyun-cli.rb
@@ -6,6 +6,7 @@ class AliyunCli < Formula
   homepage "https://js.work"
   url "https://aliyuncli.alicdn.com/aliyun-cli-macosx-3.0.32-amd64.tgz?spm=a2c4g.11186623.0.0.3c185444V5uI66&file=aliyun-cli-macosx-3.0.32-amd64.tgz"
   license "MIT"
+  version "1.0.0"
 
   def install
     chmod 0755, "aliyun"
