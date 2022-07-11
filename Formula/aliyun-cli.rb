2473efb5052447b48372fbbcefd1210bdc9efe87
afeiship <1290657123@qq.com>
2022-07-11 21:50:31 +0800

    fix: macos version

--- Formula/aliyun-cli.rb
+++ Formula/aliyun-cli.rb
@@ -4,7 +4,7 @@
 class AliyunCli < Formula
   desc "Aliyun cli for ali oss."
   homepage "https://js.work"
-  url "https://aliyuncli.alicdn.com/aliyun-cli-linux-3.0.32-amd64.tgz?spm=a2c4g.11186623.0.0.3c185444V5uI66&file=aliyun-cli-linux-3.0.32-amd64.tgz"
+  url "https://aliyuncli.alicdn.com/aliyun-cli-macosx-3.0.32-amd64.tgz?spm=a2c4g.11186623.0.0.3c185444V5uI66&file=aliyun-cli-macosx-3.0.32-amd64.tgz"
   license "MIT"
 
   def install
