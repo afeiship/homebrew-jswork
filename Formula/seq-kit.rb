6becfc140868fa76862f1a66b0adeb99d94df912
afeiship <1290657123@qq.com>
2022-07-13 09:08:51 +0800

    feat: seqkit

---
+++ Formula/seq-kit.rb
@@ -0,0 +1,16 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+# https://help.aliyun.com/document_detail/139508.html
+
+class SeqKit < Formula
+  desc "A cross-platform and ultrafast toolkit for FASTA/Q file manipulation in Golang."
+  homepage "https://js.work"
+  url "https://github.com/shenwei356/seqkit/releases/download/v2.2.0/seqkit_darwin_amd64.tar.gz"
+  license "MIT"
+  version "1.0.0"
+
+  def install
+    bin.install "seqkit"
+  end
+end
