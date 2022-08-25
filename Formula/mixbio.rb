2d3e8a789c24d7fd8c0b6a6c49887c88585f1d58
afeiship <1290657123@qq.com>
2022-08-25 20:52:24 +0800

    feat: add commet repo url

--- Formula/mixbio.rb
+++ Formula/mixbio.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-# https://help.aliyun.com/document_detail/139508.html
+# https://github.com/alo7i/mixbio-cli
 
 class Mixbio < Formula
   desc "Mixbio cli."
